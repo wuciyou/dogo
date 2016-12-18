e273d5dc1b35220d8f3b895641a57a127ec13f71
握了根草 <898060380@qq.com>
2016-12-18 21:25:48 +0800

    添加finish管道负责将数据写入到浏览器

---
+++ pipeline_finish_request.go
@@ -0,0 +1,15 @@
+package dogo
+
+type pipelineFinishRequest struct {
+}
+
+func (f *pipelineFinishRequest) PipelineRun(ctx *Context) bool {
+
+	data := make([]byte, ctx.response.writeBuf.Len())
+
+	ctx.response.writeBuf.Read(data)
+	ctx.response.rw.Write(data)
+
+	DogoLog.Infof("response data:%s, byte:%v ", string(data), data)
+	return true
+}
